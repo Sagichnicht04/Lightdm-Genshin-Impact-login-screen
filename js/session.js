3ea2f0df96da3f6d660c0d1688926a69bbb3a6b4
sagichnicht04 <florian.zuerner@gmail.com>
2024-06-19 15:07:51 +0200

    Beautiful session selecter 	modified:   css/dialog.css 	new file:   desktop_environment.svg 	modified:   index.html 	modified:   js/session.js

--- js/session.js
+++ js/session.js
@@ -7,24 +7,49 @@ async function startSession(){
     window.lightdm.start_session(session);
 }
 
-function switchSession(){
-    openDialog();
-    document.querySelector("#login_section").setAttribute("hidden","hidden");
-    document.querySelector("#sessions_section").removeAttribute("hidden");
-    let select = document.querySelector("#sessions_select");
+function toggleSessionDialog(){
+    if(document.querySelector("#sessions_section").hasAttribute("hidden")){
+        openDialog();
+        document.querySelector("#login_section").setAttribute("hidden","hidden");
+        document.querySelector("#sessions_section").removeAttribute("hidden");
+        let select = document.querySelector("#sessions_select");
 
+        if(select.innerHTML == "") setSessionButtonText(select,window.lightdm.sessions[0].name);
+    }
+    else{
+        openDialog();
+        document.querySelector("#sessions_section").setAttribute("hidden","hidden");
+        document.querySelector("#login_section").removeAttribute("hidden");
+        session = window.lightdm.sessions[document.querySelector("#sessions_select").getAttribute("data-session")].key;
+    }
+}
+function switchSession(){
     let index=0;
+    let div = document.querySelector("#session_options");
     window.lightdm.sessions.forEach(session => {
-    //["default","not_default","dontusethis"].forEach(session => {
-        let option = document.createElement("option");
-        option.setAttribute("value", index++);
-        option.innerText = session.name;
-        select.appendChild(option);
+        let option = document.createElement("button");
+        option.setAttribute("data-session", index++);
+        option.setAttribute("onclick","sessionSelected(this)");
+        setSessionButtonText(option, session.name);
+        div.appendChild(option);
     });
 }
-function sessionSelected(){
-    openDialog();
-    document.querySelector("#sessions_section").setAttribute("hidden","hidden");
-    document.querySelector("#login_section").removeAttribute("hidden");
-    session = window.lightdm.sessions[document.querySelector("#sessions_select").value].key;
+function sessionSelected(input){
+    let select = document.querySelector("#sessions_select");
+    setSessionButtonText(select, window.lightdm.sessions[input.getAttribute("data-session")].name);
+    select.setAttribute("data-session",input.getAttribute("data-session"));
+    document.querySelectorAll('#session_options > button:not(:first-child)').forEach(element=>{
+        element.remove();
+    })
+}
+
+function setSessionButtonText(button, text){
+    button.innerHTML = "<img src='desktop_environment.svg'> | "+text;
+    button.innerHTML = `
+    <div>
+        <img src="desktop_environment.svg">
+        <span> | </span>
+        <span>`+text+`</span>
+    </div>
+    `
 }
